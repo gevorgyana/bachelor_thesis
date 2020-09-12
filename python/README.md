fe7de31a041e2ae14d86d8944b4e0f0e34142105
Artyom Gevorgyan <artemhevorhian@gmail.com>
2020-09-12 14:19:30 +0300

    Milestone 1 - form a grammar of the words in the downloaded dataset, then use it to recognize a simple language.

--- python/README.md
+++ python/README.md
@@ -3,3 +3,6 @@ This is a Python solution. It is divided into:
 - processing wav files to obtain a transcript of them
 
 - processing the transcript to obtain a program, valid in Python
+
+Curretnly experimenting with this dataset, which should be inflated inside
+of data/ directory to reproduce the behaviour of the scripts.
--- python/transcribe.py
+++ python/transcribe.py
@@ -1,3 +1,10 @@
 import librosa
 
-# to recognize speech, we need a lot of words.
+# to recognize speech, we need the words that are in the alphabet of the
+# language. We donwload a sample dataset from here:
+# download.tensorflow.org/data/speech_commands_v0.01.tar.gz
+# and inflate it into the data/ directory.
+# This dataset simple words like 'cat', 'dog', etc., spoken by native
+# speakers. We can construct a grammar from this simple set of words
+# that would correspond to the grammar of Python, for example, or its
+# small subset.
