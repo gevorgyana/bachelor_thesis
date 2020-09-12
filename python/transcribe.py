cddc103368008d1031f9b06664081e5d6b87c51f
Artyom Gevorgyan <artemhevorhian@gmail.com>
2020-09-12 15:16:20 +0300

    Chores(add .gitignore)

--- python/transcribe.py
+++ python/transcribe.py
@@ -3,14 +3,14 @@
 import librosa, librosa.display
 import matplotlib.pyplot as plt
 
-# to recognize speech, we need the words that are in the alphabet of the
+# To recognize speech, we need the words that are in the alphabet of the
 # language. We donwload a sample dataset from here:
 # download.tensorflow.org/data/speech_commands_v0.01.tar.gz
 # and inflate it into the data/ directory.
-# This dataset simple words like 'cat', 'dog', etc., spoken by native
-# speakers. We can construct a grammar from this simple set of words
-# that would correspond to the grammar of Python, for example, or its
-# small subset.
+# This dataset contains simple words like 'cat', 'dog', etc., spoken by
+# native speakers. We can construct a grammar from this simple set of
+# words that would correspond to the grammar of Python, for example, or
+# its small subset.
 
 def mel_spectrogram(path: str):
     # at the default sample rate = 22050 HZ
