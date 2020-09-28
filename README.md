c992614a12687a0553116560a83a7677776985d5
Artyom Gevorgyan <artemhevorhian@gmail.com>
2020-09-28 15:08:28 +0300

    Everything works, will use a ConvNet now and see the results.

--- README.md
+++ README.md
@@ -1,4 +1,3 @@
 Started working on speech recognition for interpreting Python commands.
 
-The goals of this project are now updated. TODO describe the whole idea
-here.
+TODO. Describe the big picture here.
--- python/transcribe.py
+++ python/transcribe.py
@@ -7,8 +7,8 @@ import matplotlib.pyplot as plt
 import librosa.display
 import numpy as np
 import json
-
-print("!Done with imports")
+import functools
+import itertools
 
 def one_hot_labels(labels: []):
     """
@@ -17,7 +17,6 @@ def one_hot_labels(labels: []):
     sz = len(labels)
     output = {}
 
-    # how to do it?
     for i, val in enumerate(labels):
         one_hot_i = [0 for i in range(sz)]
         one_hot_i[i] = 1
@@ -26,59 +25,55 @@ def one_hot_labels(labels: []):
     return output
 
 def train_model_digits_from_wav():
+    NUM_CLASSES = 10
+    NUM_EPOCHS = 50
     """
     Do multi-label classification
     """
+    # --- Prepare data
     data = {}
     with open('data.json', 'r') as in_json:
         data = json.load(in_json)
 
-    # convert to one-hot
+    # Convert to one-hot
     conversion = one_hot_labels(
         set(data['category'])
     )
     category = []
-    # todo itertools map? :D
     for i in data['category']:
         category.append(conversion[i])
     category = np.array(category)
     print(category)
-
     data['mfcc'] = np.array(data['mfcc'], dtype = np.float32)
 
-    # 10 digits
-    assert data['mfcc'].shape[0] == 10 * preprocess.THRESHOLD
-    # we need the most informative features we can get, 40 is the
-    # maximum value for librosa.feature.mfcc
+    # Dimensionality checks to find bugs
+    assert data['mfcc'].shape[0] == NUM_CLASSES * preprocess.THRESHOLD
+    # 40 is the maximum value for librosa.feature.mfcc
     assert data['mfcc'].shape[1] == 40
-    # the next dimension can vary, depending on how fine-grained
-    # SFFT was.
+    # The other dimension can vary, depending on how fine-grained SFFT was.
 
-    # Now train a simple model
-    inputs = keras.layers.Input(shape =
-                                (data['mfcc'].shape[1],
-                                 data['mfcc'].shape[2]))
+    # --- Build a simple model
+    inputs = keras.layers.Input(shape = (data['mfcc'].shape[1], data['mfcc'].shape[2]))
     layer = keras.layers.Flatten()(inputs)
-
     layer = keras.layers.Dense(32, activation = 'relu') (layer)
-    # REFACTOR
-    NUM_CLASSES = 10
     layer = keras.layers.Dense(NUM_CLASSES, activation = 'softmax') (layer)
+    model = keras.Model(inputs = inputs, outputs = layer)
 
-    model = keras.Model(
-        inputs = inputs,
-        outputs = layer
-    )
-
-    model.compile(
-        loss = 'categorical_crossentropy'
-    )
+    # --- Train a simple model
+    model.compile(loss = 'categorical_crossentropy')
     model.summary()
+    model.fit(data['mfcc'], category, epochs = NUM_EPOCHS)
 
-    # REFACTOR
-    NUM_EPOCHS = 50
-    model.fit(
-        data['mfcc'], category, epochs = NUM_EPOCHS
-    )
+    # --- Check the model - todo use evaluation instead with train_test_split instead, also use validation/evaluation data instead of training data
+    assert functools.reduce(
+        lambda x, y: x + y,
+        itertools.chain(
+            *list(map(
+                lambda x: [0 if i < 0.5 else 1 for i in x],
+                model.predict(
+                    data['mfcc']
+                ) - category
+            )))
+    ) == 0
 
 train_model_digits_from_wav()
