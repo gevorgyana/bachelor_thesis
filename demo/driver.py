002561702b9d8e5353f144c30ff8f3cd4b0858c9
Artyom Gevorgyan <43173333+gevorgyana@users.noreply.github.com>
2022-06-17 14:30:20 +0300

    Update driver.py

--- demo/driver.py
+++ demo/driver.py
@@ -5,6 +5,8 @@ cp = subprocess.run(['clang', '-fsyntax-only', '-Xclang', '-ast-dump', 'main.cpp
 
 chunks = str(cp.stdout.decode('utf-8')).split('\n')
 
+result = "(defun highlight()(interactive)"
+
 for i in chunks:
     if str.__contains__(i, "CXXRecordDecl") and str.__contains__(i, "definition"):#i.contains("CXXRecordDecl"):
         S = re.search("<.*>", i)
@@ -13,5 +15,25 @@ for i in chunks:
         from_line = re.search("[^:]*", from_).group()
         from_col = from_[len(from_line) + 1:]
         print (from_line, from_col)
-        # Emacs Lisp command
-        #print ()
+        
+        # find positions for ELisp expression
+        offset_in_chars = 0
+
+        f = open('main.cpp', 'r')
+        lines = f.readlines()
+        for (idx, line) in enumerate(lines):
+            print (idx, line)
+            if idx + 1 == from_line:
+                break
+            else:
+                offset_in_chars += len(line)
+
+        # print('offset {}'.format(offset_in_chars))
+        result += '(overlay-put (make-overlay 7 41) \'face \'highlight)(overlay-put (make-overlay 50 65) \'face \'highlight)'
+        
+        # result += '(overlay-put (make-overlay 1 2) \'face \'highlight)'.format(int(offset_in_chars) + int(from_col))
+        
+result += ')'
+
+with open('decoded', 'w') as f:
+    f.write(result)
